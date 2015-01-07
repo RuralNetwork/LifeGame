473d358605e6e9357febff3fe01bae8223b2fb99
Ricky96 <rickyzaglia@gmail.com>
2015-01-07 19:44:25 +0100

    Inizio del progetto

---
+++ LifeGame/Cell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame
+{
+    struct PointF
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public PointF(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    enum Terrain
+    {
+        Grass,
+        Wood,
+        River,
+        Snow
+    }
+
+    class HexGrid
+    {
+
+    }
+
+    class Cell
+    {
+        HexGrid parent;
+
+        public readonly PointF Position { get; set; }
+        public Terrain Terrain { get; set; }
+        public int Altitude { get; set; }
+        public List<Item> SubItems { get; set; }
+
+        public Cell(HexGrid parent, float x, float y)
+        {
+            this.parent = parent;
+            Position = new PointF(x, y);
+        }
+    }
+
+    class Item
+    {
+
+    }
+}
