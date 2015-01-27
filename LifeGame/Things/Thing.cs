538fde4504c30c9896548d9da042e6f582be2616
apra <samuele.papa@gmail.com>
2015-01-27 22:47:14 +0100

    added comments in Thing

--- LifeGame/Things/Thing.cs
+++ LifeGame/Things/Thing.cs
@@ -18,9 +18,13 @@ namespace LifeGame
         public abstract float R { get; }
         public abstract float G { get; }
         public abstract float B { get; }
+        //Shouldn't this be speed? it's a float, if you set every input as a number between 0 and 1,
+        //but this one is either 0 or 1, the network won't understand that easily what is going on
+        //even though NN are not understable by humans, so mine is just a guess based on my experiments
         public abstract float Moving { get; }
 
         //feel
+        //Since it's a sensation and not a state, it sould be Pain and Temperature, not Painful and Warmth
         public abstract float Painful { get; }
         public abstract float Weight { get; }
         public abstract float Warmth { get; }
